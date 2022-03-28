09862e1bd3b4c9a065a00b2e1f24ca319eca9ffa
saa1saar <salli.saarinen@edu.turkuamk.fi>
2022-03-28 12:31:53 +0300

    Added comment universityClass.py

--- Classes/universityClass.py
+++ Classes/universityClass.py
@@ -1,3 +1,4 @@
 # File name: universityClass.py
 # Author: (use your name, of course)
-# Description: Defines the attributes of the university.
+# Description: Defines the attributes of the university. 
+# University has multiple teachers and students.
