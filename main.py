18584bc60945152480b29d8fd15bf4bba8a31fd9
Otto <ottolaitinen@outlook.com>
2022-03-28 13:01:00 +0300

    basic main

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # File name: main.py
 # Author: (use your name, of course)
 # Description: Main function for the university lottery.
+
+
+def main():
+    students = []
+    teachers = []
+    other_people = []
+    all_contestants = students + teachers + other_people
+    lottery_tickets = []
+
+    # function here to define the winner
+
+
+main()
