1ae4a3b642a953ff043ae1b10f245d2c756cc493
Otto <ottolaitinen@outlook.com>
2022-03-28 13:09:10 +0300

    basics on main

--- main.py
+++ main.py
@@ -2,8 +2,19 @@
 # Author: (use your name, of course)
 # Description: Main function for the university lottery.
 
+from os import path
+import sys
+
+sys.path.append(path.abspath("./Classes"))
+
+from Classes.personClass import Person
+from Classes.universityClass import University
+
 
 def main():
+    university = University("TUAS")  # test
+    print(university.name)
+
     students = []
     teachers = []
     other_people = []
