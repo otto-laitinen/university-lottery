30a19ac835a772991fc6caa9f1569bb78fe610aa
Otto <ottolaitinen@outlook.com>
2022-03-28 13:30:15 +0300

    add instance of ticket

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from Classes.personClass import Person
 from Classes.studentClass import Student
 from Classes.teacherClass import Teacher
 from Classes.universityClass import University
-
-# from Classes.lotteryticketClass import
+from Classes.lotteryticketClass import Lotteryticket
 
 
 def main():
@@ -27,7 +26,8 @@ def main():
     for i in all_contestants:
         print(i.get_first_name())
 
-    # lottery_tickets = []
+    lottery_tickets = [Lotteryticket]
+    print(lottery_tickets[0])
 
     # function here to define the winner
 
