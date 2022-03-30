497add6475d115946bdb9ffc4ab33c1ec786718e
Thomás <thomas.rizziomura@edu.turkuamk.fi>
2022-03-30 12:49:28 +0300

    Merge branch 'main' of https://github.com/otto-laitinen/university-lottery

--- Classes/personClass.py
+++ Classes/personClass.py
@@ -9,9 +9,12 @@ class Person:
         first_name,
         last_name,
         age,
+        tickets,
     ):
         self.set_first_name(first_name)
         self.set_last_name(last_name)
+        self.set_age(age)
+        self.set_tickets(tickets)
 
     def set_first_name(self, first_name):
         self.first_name = first_name
@@ -30,3 +33,11 @@ class Person:
 
     def get_age(self):
         return self.age
+
+    def set_tickets(self, tickets):
+        self.tickets = tickets
+    
+    def get_tickets(self):
+        return self.tickets 
+
+
